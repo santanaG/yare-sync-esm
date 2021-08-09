d41653813ef83edbded8146467121ac378b98cc6
santanaG <gs8777@gmail.com>
2021-08-09 00:32:24 -0400

    refactors login.js

--- src/login.js
+++ src/login.js
@@ -3,16 +3,10 @@ import axios from 'axios'
 
 const [dun, dpw] = ['noUsername', 'noPassword']
 
-const login = (user_name = dun, password = dpw) => axios
+export default async (user_name = dun, password = dpw) => await axios
   .post('https://yare.io/validate', { user_name, password }, { timeout: 5000 })
-  .catch(({ code }) => {
-    console.log(`Login failed with ${code}`)
+  .then(({ data: { user_id, data: session_id } }) => ({ user_id, session_id }))
+  .catch(({ response }) => {
+    console.log(`Login attempt failed with ${response?.statusText} status`)
     console.log(`Username/Password: ${user_name}/${password}`)
-    throw new Error('Login attempt failed!')
   })
-
-export default async (un, pw) => {
-  const { data: { user_id, data: session_id } } = await login(un, pw)
-
-  return { user_id, session_id }
-}
