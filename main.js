788e70afe297497124a46aabebe6f22af368bd65
santanaG <gs8777@gmail.com>
2021-08-08 20:09:25 -0400

    adds login

---
+++ main.js
@@ -0,0 +1,3 @@
+import login from './src/login.js'
+
+export default { login }
---
+++ src/login.js
@@ -0,0 +1,18 @@
+/* eslint-disable camelcase */
+import axios from 'axios'
+
+const { dun, dpw } = ['noUsername', 'noPassword']
+
+const login = (user_name = dun, password = dpw) => axios
+  .post('https://yare.io/validate', { user_name, password }, { timeout: 5000 })
+  .catch(({ code }) => {
+    console.log(`Login failed with ${code}`)
+    console.log(`Username/Password: ${user_name}/${password}`)
+    throw new Error('Login attempt failed!')
+  })
+
+export default async (un, pw) => {
+  const { data: { user_id, data: session_id } } = await login(un, pw)
+
+  return { user_id, session_id }
+}
