65c7a3054d9413a1a395d209dab43a0ee9a4d122
santanaG <gs8777@gmail.com>
2021-08-12 00:56:13 -0400

    removes call to isActiveSession

--- src/sendCode.js
+++ src/sendCode.js
@@ -1,18 +1,14 @@
 /* eslint-disable camelcase */
 import WebSocket from 'ws'
-import isActiveSession from './isActiveSession.js'
 
 const ws = game => new WebSocket(`wss://yare.io/d1/${game}`)
 
-export default (code, account) => async game => (
-  await isActiveSession(account) && await new Promise(resolve => {
-    const
-      session = ws(game),
-      { user_id: u_id, session_id } = account,
-      codeData = JSON.stringify({ u_code: code, u_id, session_id })
+export default (code, { user_id: u_id, session_id }) => async game => await new Promise(resolve => {
+  const
+    session = ws(game),
+    codeData = JSON.stringify({ u_code: code, u_id, session_id })
 
-    session
-      .on('open', _ => resolve(!(session.send(codeData) || session.close())))
-      .on('error', _ => resolve(false))
-  })
-)
+  session
+    .on('open', _ => resolve(!(session.send(codeData) || session.close())))
+    .on('error', _ => resolve(false))
+})
