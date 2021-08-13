d9fa26b5b88710852dce19b51fa3a1cb6e4bb7cb
santanaG <gs8777@gmail.com>
2021-08-13 08:19:26 -0400

    small changes + unnecessary async/await

--- src/sendCode.js
+++ src/sendCode.js
@@ -5,10 +5,9 @@ const
   ws = game => new WebSocket(`wss://yare.io/d1/${game}`),
   { toSnakeCaseSendCode: toSnakeCase } = namingHelper
 
-export default (code, { user, session }) => async game => await new Promise(resolve => {
-  const
-    session = ws(game),
-    codeData = JSON.stringify(toSnakeCase(code, user, session))
+export default (code, { user, session }) => game => new Promise(resolve => {
+  const wsSession = ws(game)
+  const codeData = JSON.stringify(toSnakeCase(code, user, session))
 
   wsSession
     .on('open', _ => resolve(!(wsSession.send(codeData) || wsSession.close())))
